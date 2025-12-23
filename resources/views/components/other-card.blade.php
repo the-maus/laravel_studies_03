02d2fc5677741384bbf4af25a26dae0fe89ce54c
the-maus <msampaio.mail@gmail.com>
2025-12-23 20:09:35 -0300

    Introduction to components slots

---
+++ resources/views/components/other-card.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <h3>This is the component's initial text</h3>
+
+    {{-- show slot content here with blade directive --}}
+    {{ $slot }}
+
+    <h3>This is the component's final text</h3>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.main_layout')
 @section('content')
 
-<div class="text-center">
+{{-- <div class="text-center">
     @foreach ($person_languages as $person => $languages)
-        {{-- component --}}
         <x-person-card :$person :$languages/>
     @endforeach
+</div> --}}
+
+{{-- components and slots --}}
+<div></div>
+    <h4 class="text-info">How does a slot work?</h4>
+    {{-- the content inside the component's tags is shown on the $slot variable inside the component template  --}}
+    <x-other-card> 
+        <h1 class="text-danger">This is the Slot!</h1>
+    </x-other-card>
 </div>
 
 @endsection
