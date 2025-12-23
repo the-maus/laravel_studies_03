a8f89c9946008d66162a5ef8244c58c7c1ed8d31
the-maus <msampaio.mail@gmail.com>
2025-12-23 20:22:25 -0300

    Component with multiple slots

---
+++ resources/views/components/multi-slot.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h4>Component with multiple slots</h4>
+
+    <p>{{ $title }}</p>
+    <p>{{ $content }}</p>
+    <div>{{ $footer }}</div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,25 @@
 </div> --}}
 
 {{-- components and slots --}}
-<div></div>
+{{-- the content inside the component's tags is shown on the $slot variable inside the component template  --}}
+{{-- <div></div>
     <h4 class="text-info">How does a slot work?</h4>
-    {{-- the content inside the component's tags is shown on the $slot variable inside the component template  --}}
     <x-other-card> 
         <h1 class="text-danger">This is the Slot!</h1>
     </x-other-card>
-</div>
+</div> --}}
+
+{{-- multiple slot --}}
+<x-multi-slot>
+    <x-slot:title>This is the title</x-slot>
+    <x-slot:content>This is the content</x-slot>
+    <x-slot:footer>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </x-slot>
+</x-multi-slot>
 
 @endsection
